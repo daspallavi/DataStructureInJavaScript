052e03ad4e52f67ab4c443b0c640f53f2b74d2c9
Pallavi Das <pallavids92@gmail.com>
2022-06-03 12:15:19 +0530

    updated code

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -24,6 +24,18 @@ class SinglyLinkedList {
     }
 
     push(val) {
-
+        var newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head
+        }
+        else {
+            this.tail.next = newNode;
+            this.tail = newNode
+        }
     }
 }
+
+var list = new SinglyLinkedList();
+list.push("apples");
+console.log("list", list);
