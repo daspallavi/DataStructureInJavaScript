c20897ef32de5640ab765324397f94e0f25b12d3
Pallavi Das <pallavids92@gmail.com>
2022-07-07 18:20:16 +0530

    stack implementation

---
+++ stackWithArrays.js
@@ -0,0 +1,15 @@
+let stack = [];
+
+stack.push("google");
+stack.push("instagram");
+stack.push("youtube");
+
+stack.pop();
+
+stack.push("amazon");
+stack.pop();
+
+/** adding in front */
+stack.unshift("added in front")
+
+stack.shift('removed from front');
