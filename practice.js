0c05e418c4a843924b2dbb4bd4eef0e9de3a77ae
Pallavi Das <pallavids92@gmail.com>
2022-07-25 01:14:41 +0530

    tree bsf traversal

--- practice.js
+++ practice.js
@@ -6,4 +6,21 @@ function x() {
     var i = 1;
 }
 
-x();
+//x();
+
+let a = [{ name: "Jim", link: "https://www.w3schools.com", subitmes: [{ name: "Jim", link: "https://www.w3schools.com" }] }, { name: "Jim", link: "https://www.w3schools.com", subitmes: [{ name: "Jim", link: "https://www.w3schools.com" }] }]
+
+
+function n() {
+    //var i = 1;
+    for (let i = 0; i <= 5; i++) {
+        setTimeout(function () {
+            console.log(i)
+        }, i * 1000)
+    }
+
+
+    console.log('hello')
+}
+
+n();
