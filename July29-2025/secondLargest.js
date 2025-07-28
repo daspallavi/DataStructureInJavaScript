50fcd17f725ff24c44421527c91e193662d97f26
Pallavi Das <pallavids92@gmail.com>
2025-07-29 00:53:34 +0530

    Solved second largest number

--- July29-2025/secondLargest.js
+++ July29-2025/secondLargest.js
@@ -5,3 +5,26 @@ console.log("------------Find the second largest in the given array-------------
  */
 
 
+let input = [4, 9, 0, 2, 8, 7, 2, 1]
+
+function getSecondLargest(arr) {
+
+    let firstLargest = -Infinity;
+    let secondLargest = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] > firstLargest) {
+            secondLargest = firstLargest;
+            firstLargest = arr[i];
+        }
+        else if (arr[i] > secondLargest) {
+            secondLargest = arr[i]
+        }
+    }
+
+    return secondLargest;
+
+}
+
+console.log(getSecondLargest(input));
