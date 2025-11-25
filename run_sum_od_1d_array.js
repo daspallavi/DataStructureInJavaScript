2c5c2774cfe7af3ce4e88782af3b39dcb6fbf5e9
Pallavi Das <pallavids92@gmail.com>
2025-11-25 18:15:37 +0530

    added new files

---
+++ run_sum_od_1d_array.js
@@ -0,0 +1,10 @@
+var runningSum = function (nums) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        let temp = nums[i] + nums[i + 1]
+        nums[i + 1] = temp;
+    }
+    return nums;
+};
+
+
+console.log(runningSum([1, 2, 1]));
