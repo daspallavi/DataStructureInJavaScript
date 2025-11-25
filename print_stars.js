1f6b5067662a0bbda46e61851af5cd8716e7e10e
Pallavi Das <pallavids92@gmail.com>
2025-11-25 23:33:19 +0530

    star message

---
+++ print_stars.js
@@ -0,0 +1,12 @@
+
+let n = 4;
+for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n - (i + 1); j++) {
+        row += "_";
+    }
+    for (let k = 0; k < i + 1; k++) {
+        row += "*";
+    }
+    console.log(row);
+}
