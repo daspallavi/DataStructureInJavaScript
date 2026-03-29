ed0181b0cb7b442a77733bbba642e82b64b75047
Pallavi Das <pallavids92@gmail.com>
2026-03-29 15:05:32 +0530

    adding another approach for contains duplicate with sliding window approach

--- containsDuplicates2.js
+++ containsDuplicates2.js
@@ -41,3 +41,33 @@ let duplicatesNearby = function (nums, k) {
 }
 
 console.log(duplicatesNearby([1,2,3,1], 3));
+
+//O(n) time complexity and O(n) space complexity
+
+/** Approach 2:
+ * Using Set and sliding window approach
+ * We can maintain a sliding window of size k and check if there are any duplicates within that window. 
+ * We can use a Set to store the elements in the current window. 
+ * As we iterate through the array, we add the current element to the Set and check if it already exists in the Set. 
+ * If it does, we return true. If the size of the Set exceeds k, we remove the element that is sliding out of the window.
+ * This approach has a time complexity of O(n) and a space complexity of O(k).
+ */
+
+
+const containsNearbyDuplicate2 =(nums,k) =>{
+
+let set = new Set();
+for(let i =0; i < nums.length; i++){
+    if(set.has(nums[i])){
+        return true;
+    }
+    set.add(nums[i]);
+    if(set.size > k){
+        set.delete(nums[i-k]);
+    }
+}
+return false;
+}
+
+
+console.log(containsNearbyDuplicate2([1,0,1,1], 1));
